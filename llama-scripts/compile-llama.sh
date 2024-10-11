57acb4ded7153b7daee4d114a4b3c8a9a17e8897
aviator19941 <avinash.sharma@amd.com>
2024-10-11 11:48:31 -0500

    Dump executable-files
    Signed-off-by: aviator19941 <avinash.sharma@amd.com>

--- llama-scripts/compile-llama.sh
+++ llama-scripts/compile-llama.sh
@@ -21,7 +21,7 @@ set -x
 
 rm -rf "${WORKING_DIR}/configurations/llama"
 rm -rf "${WORKING_DIR}/intermediates/llama"
-rm -rf "${WORKING_DIR}/sources/llama"
+rm -rf "${WORKING_DIR}/files/llama"
 rm -rf "${WORKING_DIR}/binaries/llama"
 rm -rf "${WORKING_DIR}/benchmarks/llama"
 
@@ -29,17 +29,12 @@ rm -rf "${WORKING_DIR}/benchmarks/llama"
   "${INPUT_IR}" \
   --iree-hal-dump-executable-configurations-to="${WORKING_DIR}/configurations/llama" \
   --iree-hal-dump-executable-intermediates-to="${WORKING_DIR}/intermediates/llama" \
-  --iree-hal-dump-executable-sources-to="${WORKING_DIR}/sources/llama" \
+  --iree-hal-dump-executable-files-to="${WORKING_DIR}/files/llama" \
   --iree-hal-dump-executable-binaries-to="${WORKING_DIR}/binaries/llama" \
   --iree-hal-dump-executable-benchmarks-to="${WORKING_DIR}/benchmarks/llama" \
-  --iree-scheduling-dump-statistics-file="${WORKING_DIR}/tmp/llama_scheduling_stats.txt" \
-  --iree-scheduling-dump-statistics-format=csv \
   -o "${WORKING_DIR}/llama.vmfb" \
   "$@"
 
   #--iree-hal-benchmark-dispatch-repeat-count=20 \
   #--iree-hal-executable-debug-level=3 \
-  #--iree-vulkan-target-triple=rdna3-unknown-linux \
-  #--iree-llvmcpu-target-triple=x86_64-unknown-linux \
-  #--iree-hal-cuda-llvm-target-arch=sm_80 \
   #--mlir-disable-threading \
